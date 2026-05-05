54267cf87d0d9d42eba4b3a6d413d38919f53df0
DolphlinkRoyli <roygto2013@gmail.com>
2026-05-06 02:49:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'dolphlink-v8';
+const CACHE_VERSION = 'dolphlink-v9';
 const SCOPE = self.registration && self.registration.scope
   ? new URL(self.registration.scope).pathname
   : '/Dolphlink-MainPage/';
