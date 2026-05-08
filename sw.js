b78a26b5bf120516d3685bd68e647dbc2bff74c2
dolphlinkroyli <roygto2013@gmail.com>
2026-05-09 02:02:02 +0800

    light house fix

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  * ============================================================================
  */
 
-const CACHE_VERSION = 'dolphlink-v359';
+const CACHE_VERSION = 'dolphlink-v360';
 const SCOPE = self.registration && self.registration.scope
   ? new URL(self.registration.scope).pathname
   : '/Dolphlink-MainPage/';
